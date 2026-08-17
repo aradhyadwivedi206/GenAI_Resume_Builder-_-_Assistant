f381b42190158f175b3b38c97d34e753ef0f6c4b
Aradhya dwivedi <aradhyadwivedi928@gmail.com>
2026-08-18 00:23:26 +0530

    Use environment variable for backend URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {RouterProvider} from "react-router"
+import {RouterProvider} from "react-router-dom"
 import{router}from "./app.routes.jsx"
 import { AuthProvider } from './features/auth/auth.context.jsx'
 import { InterviewProvider } from './features/interview/interview.context.jsx'
