302680f20c44ead925a5897aa4f8078c939611a8
Aradhya dwivedi <aradhyadwivedi928@gmail.com>
2026-08-18 11:28:05 +0530

    fix import path casing

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import{createBrowserRouter} from "react-router-dom";
 
-import Login from "./features/auth/pages/Login.jsx"
-import Register from "./features/auth/pages/Register.jsx"
+import Login from "./features/auth/Pages/Login.jsx"
+import Register from "./features/auth/Pages/Register.jsx"
 import { Protected } from "./features/auth/components/protected.jsx";
 import Home from "./features/interview/pages/Home.jsx";
 import Interview from "./features/interview/pages/interview.jsx";
