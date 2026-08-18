7b1958ffa5a52be8049522d3fb170a2ea2035aeb
Aradhya dwivedi <aradhyadwivedi928@gmail.com>
2026-08-18 11:48:25 +0530

    chnges

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,20 @@ const cors=require("cors")
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin:"https://gen-ai-resume-builder-assistant-omega.vercel.app",credentials:true
-}))
+// app.use(cors({
+//     origin:"https://gen-ai-resume-builder-assistant-omega.vercel.app",credentials:true
+// }))
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://gen-ai-resume-builder-assi-git-b416b2-aradhya-dwivedis-projects.vercel.app",
+    "https://gen-ai-resume-builder-assistant-6knylcswj.vercel.app"
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
 // require all the routes here
 const authRouter=require('./routes/auth.routes')
 const interviewRouter=require('./routes/interview.routes')
